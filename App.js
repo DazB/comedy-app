6d3f506f8b81e4c9f78d103febdc41c3efd22c73
DazB <dazb@alumni.york.ac.uk>
2018-02-09 22:28:06 +0800

    Initial commit. Hello World you fuckers.
    "It does not matter how slowly you go as long as you do not stop." - Confucius

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World!!!!!</Text>
+      </View>
+    );
+  }
+}
